e413d24a84ff6e30d793c634ea8d1526bd9c606c
Noah Söng <noah.song@elev.ga.ntig.se>
2023-10-09 08:56:33 +0200

    har gjort uppg b)

--- src/App.java
+++ src/App.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 public class App {
 public static void main(String[] args)
 {
-Scanner tangentbord=new Scanner(System.in);
-System.out.println("Skriv in första talet:");
-int tal1=tangentbord.nextInt();
-System.out.println("Skriv in andra talet:");
-int tal2=tangentbord.nextInt();
+// Scanner tangentbord=new Scanner(System.in);
+// System.out.println("Skriv in första talet:");
+// int tal1=tangentbord.nextInt();
+// System.out.println("Skriv in andra talet:");
+// int tal2=tangentbord.nextInt();
 
-System.out.println(tal1==tal2);
-System.out.println(tal1<tal2);
-System.out.println(tal1>tal2);
+// System.out.println(tal1==tal2);
+// System.out.println(tal1<tal2);
+// System.out.println(tal1>tal2);
+
+int fält[]={4,5,2};
+System.out.println((fält[0]>fält[1])+" "+(fält[0]>fält[2]));
+System.out.println((fält[0]<fält[1])+" "+(fält[0]<fält[2]));
+System.out.println((fält[0]==fält[1])+" "+(fält[0]==fält[2]));
  }
 }
